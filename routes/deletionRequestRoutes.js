0599b81991c1cdff6d0439219316255a363e8309
saurabh1784 <noori7995@gmail.com>
2026-07-25 08:09:17 +0530

    new delation rewu

--- routes/deletionRequestRoutes.js
+++ routes/deletionRequestRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const deletionRequestController = require('../controllers/deletionRequestController');
-const { webOrJwtAuth } = require('../middleware/auth');
+const { webOrJwtAuth } = require('../middleware/webOrJwtAuth');
 
 const router = express.Router();
 
